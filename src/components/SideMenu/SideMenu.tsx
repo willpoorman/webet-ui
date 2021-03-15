e959924567ea11a390626fb3ae87d0c86b54bc5b
Kosma Olan <kosmaol@gmail.com>
2021-03-14 21:54:16 -0500

    refactor: centralized useStyle

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -13,31 +13,7 @@ import {
   withRouter,
   RouteComponentProps,
 } from "react-router-dom";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-
-const drawerWidth = 240;
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: "flex",
-    },
-    drawer: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-    drawerPaper: {
-      width: drawerWidth,
-    },
-    nested: {
-      paddingLeft: theme.spacing(4),
-    },
-    content: {
-      flexGrow: 1,
-      backgroundColor: theme.palette.background.default,
-      padding: theme.spacing(3),
-    },
-  })
-);
+import { useStyles } from "../../useStyle";
 
 interface PageLinkProps extends Pick<RouterLinkProps, "to">, Pick<ListItemProps, "selected"> {
   icon?: JSX.Element | null;
