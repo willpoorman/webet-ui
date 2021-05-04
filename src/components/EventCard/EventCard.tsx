abe6f45840b3b9a96404fa6ccd78280cb3e01e11
Will Poorman <will.k.poorman@gmail.com>
2021-05-03 23:07:53 -0500

    style: Reorder imports

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -1,10 +1,10 @@
-import Typography from "@material-ui/core/Typography";
 import React, { FunctionComponent } from "react";
-import { useStyles } from "../../useStyle";
+import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { useStyles } from "../../useStyle";
 
 interface EventCardProps {
   name: string;
