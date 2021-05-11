75569f4ae61901cc40427098a0a0896cf0ab14c8
Will Poorman <will.k.poorman@gmail.com>
2021-05-10 22:59:26 -0500

    fix: Add error handling to GET call in MatchCards
    Also add loading indicator while GET call is occuring

--- src/components/MatchCards/MatchCards.tsx
+++ src/components/MatchCards/MatchCards.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
-import { Grid, Paper, Typography } from "@material-ui/core";
+import { CircularProgress, Grid, Paper, Typography } from "@material-ui/core";
 import axios from "axios";
 import { Match } from "../../models";
 import { useStyles } from "../../useStyle";
@@ -11,9 +11,14 @@ export interface MatchCardsProps {
 
 export const MatchCards: FunctionComponent<MatchCardsProps> = ({ eventId }) => {
   const classes = useStyles();
+  const [loading, setLoading] = useState(true);
+  const [errorOccurred, setErrorOccurred] = useState(false);
   const [matches, setMatches] = useState<Match[] | []>([]);
 
   useEffect(() => {
+    setLoading(true);
+    setErrorOccurred(false);
+
     axios
       .get<Match[]>("/api/matches", {
         params: {
@@ -22,38 +27,52 @@ export const MatchCards: FunctionComponent<MatchCardsProps> = ({ eventId }) => {
       })
       .then((response) => {
         setMatches(response.data);
-      });
+      })
+      .catch((error) => {
+        console.error(error);
+        setErrorOccurred(true);
+      })
+      .finally(() => setLoading(false));
   }, [eventId]);
 
-  const cards = matches.map((match: Match) => {
-    const matchDescription = (
-      <div>
-        {match.tags.join(", ")}
-        <p>{match.notes}</p>
-      </div>
-    );
+  let content = <CircularProgress />;
+  if (errorOccurred) {
+    content = <div>Something went wrong</div>;
+  } else if (!loading && matches !== undefined && matches !== null) {
+    const cards = matches.map((match: Match) => {
+      const matchDescription = (
+        <div>
+          {match.tags.join(", ")}
+          <p>{match.notes}</p>
+        </div>
+      );
 
-    return (
-      <Grid item xs={12} md={3} sm={6} key={match.id}>
-        <MatchCard
-          id={match.id}
-          name={match.name}
-          description={matchDescription}
-          startDate={match.start_time}
-          eventId={match.event}
-        />
+      return (
+        <Grid item xs={12} md={3} sm={6} key={match.id}>
+          <MatchCard
+            id={match.id}
+            name={match.name}
+            description={matchDescription}
+            startDate={match.start_time}
+            eventId={match.event}
+          />
+        </Grid>
+      );
+    });
+
+    content = (
+      <Grid container spacing={3}>
+        {cards}
       </Grid>
     );
-  });
+  }
 
   return (
     <Paper className={classes.paper}>
       <Typography variant="h2" gutterBottom>
         Matches
       </Typography>
-      <Grid container spacing={3}>
-        {cards}
-      </Grid>
+      {content}
     </Paper>
   );
 };
